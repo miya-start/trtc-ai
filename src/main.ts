2382530ce7a94ebc78f6d618aa2939512a46a2c8
miya-start <0000000to1@gmail.com>
2022-12-19 09:49:04 +0900

    feat: Tutorial: Quick Start Call
    https://web.sdk.qcloud.com/trtc/webrtc/doc/en/tutorial-11-basic-video-call.html

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,40 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import TRTC, { Client, LocalStream } from 'trtc-js-sdk'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const sdkAppId = import.meta.env.VITE_SDKAPPID | 0
+const userSig = import.meta.env.VITE_USERSIG
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let roomId = 1
+let userId = '1'
+let client: Client
+let localStream: LocalStream
+document.getElementById('startCall')!.onclick = async function () {
+  roomId =
+    roomId ??
+    parseInt(document.querySelector<HTMLInputElement>('#roomId')!.value)
+  userId = userId ?? document.querySelector<HTMLInputElement>('#userId')!.value
+  client = TRTC.createClient({ mode: 'rtc', sdkAppId, userId, userSig })
+  client.on('stream-added', (event) => {
+    const remoteStream = event.stream
+    console.log('remote stream add streamId: ' + remoteStream.getId())
+    client.subscribe(remoteStream)
+  })
+  client.on('stream-subscribed', (event) => {
+    const remoteStream = event.stream
+    remoteStream.play('remoteStreamContainer')
+  })
+  try {
+    await client.join({ roomId })
+    localStream = TRTC.createStream({ userId, audio: true, video: true })
+    await localStream.initialize()
+    localStream.play('localStreamContainer')
+    await client.publish(localStream)
+  } catch (error) {
+    console.error(error)
+  }
+}
+document.getElementById('finishCall')!.onclick = async function () {
+  localStream.close()
+  await client.leave()
+  client.destroy()
+}
