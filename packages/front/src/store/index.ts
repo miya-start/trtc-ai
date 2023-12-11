0dee223a6639e5a5c907e0016fa9e08ff3ccef5c
miya-start <0000000to1@gmail.com>
2023-12-11 21:07:13 +0900

    refactor: remove unused code

--- packages/front/src/store/index.ts
+++
@@ -1,5 +0,0 @@
-export async function getData<T>(url: string): Promise<T> {
-  const res = await fetch(url)
-  if (res.ok) return res.json()
-  throw Error(`Could not fetch ${url}, status code: ${res.status}`)
-}
